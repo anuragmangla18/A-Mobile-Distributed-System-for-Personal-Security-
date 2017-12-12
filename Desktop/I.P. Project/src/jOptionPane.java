f1c6b187b60c074065b5b3b26640856bae877802
anurag038 <a8544100318@gmail.com>
2017-12-12 23:37:01 +0530

    new commit

---
+++ Desktop/I.P. Project/src/jOptionPane.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Anurag gupta
+ */
+class jOptionPane {
+
+    static void showMessageDialog(Object object, String string) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
